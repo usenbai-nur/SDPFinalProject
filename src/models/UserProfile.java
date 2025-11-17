3d729e5c3872408ce858755cfb78cee148c036da
NURA <241265@astanait.edu.kz>
2025-11-17 07:16:34 +0500

    fixed bags in models

--- src/models/UserProfile.java
+++ src/models/UserProfile.java
@@ -14,25 +14,17 @@ public class UserProfile {
     private String link = "";
 
     public UserProfile(String name, String email, int age) {
-        this.name = name; this.email = email; this.age = age;
+        this.name = name;
+        this.email = email;
+        this.age = age;
     }
 
-    public String getName() {
-        return name;
-    }
-    public String getEmail() {
-        return email;
-    }
-    public int getAge() {
-        return age;
-    }
+    public String getName() { return name; }
+    public String getEmail() { return email; }
+    public int getAge() { return age; }
 
-    public boolean isSubscribed() {
-        return subscribed;
-    }
-    public void setSubscribed(boolean s) {
-        this.subscribed = s;
-    }
+    public boolean isSubscribed() { return subscribed; }
+    public void setSubscribed(boolean s) { this.subscribed = s; }
 
     public void addBookmark(int id) {
         if (!bookmarks.contains(id)) bookmarks.add(id);
@@ -42,22 +34,16 @@ public class UserProfile {
     }
 
     public void addHistory(int id) {
-        history.add(0, id); if (history.size() > 200) history.remove(history.size()-1);
+        history.add(0, id);
+        if (history.size() > 200) history.remove(history.size() - 1);
     }
     public List<Integer> getHistory() {
         return new ArrayList<>(history);
     }
 
-    public void setSignature(String s) {
-        this.signature = s;
-    }
-    public String getSignature() {
-        return signature;
-    }
-    public void setLink(String l) {
-        this.link = l;
-    }
-    public String getLink() {
-        return link;
-    }
+    public void setSignature(String s) { this.signature = s; }
+    public String getSignature() { return signature; }
+
+    public void setLink(String l) { this.link = l; }
+    public String getLink() { return link; }
 }
